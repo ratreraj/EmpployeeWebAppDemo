2940dce47d663d56ae536a7cce19dde9f666f274
raj ratre <ratre.raj@gmail.com>
2022-06-03 08:37:17 +0530

    Add project files.

---
+++ EmpployeeWebAppDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmpployeeWebAppDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
